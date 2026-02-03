fd898f68ed47485474a473cfaa24249226e3890d
hugopereira-cs <hugoper_cs@yahoo.com>
2026-02-03 18:14:36 -0300

    fix: main height

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { OperationHistory } from './components/OperationHistory'
 function App() {
   
   return (
-    <main className="py-28 px-4 sm:px-10 lg:px-20 xl:px-32 2xl:px-64
+    <main className="py-12 px-4 sm:py-28 sm:px-10 lg:px-20 xl:px-32 2xl:px-64
       flex flex-col sm:flex-row
       items-center sm:items-stretch
       gap-6">
